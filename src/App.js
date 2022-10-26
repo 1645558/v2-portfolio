1535271f714a803ec9f29247c344e9211da50860
Rare Raptor <connorwtuncc@gmail.com>
2022-10-26 11:50:00 -0400

    added useState hooks and buttons to cycle through carouselItems

--- src/App.js
+++ src/App.js
@@ -21,8 +21,8 @@ function App() {
         <section className='two'>
           <Carousel>
             <CarouselItem>Item 1</CarouselItem>
-            <CarouselItem>Item 1</CarouselItem>
-            <CarouselItem>Item 1</CarouselItem>
+            <CarouselItem>Item 2</CarouselItem>
+            <CarouselItem>Item 3</CarouselItem>
           </Carousel>
           About
         </section>
